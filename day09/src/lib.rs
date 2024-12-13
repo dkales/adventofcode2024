c0fcc186e40e61e4d164cdb6792eab6cd54f50d9
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-12-13 23:39:26 +0100

    slow day09

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,29 +1,51 @@
 use std::{collections::VecDeque, str::FromStr};
 
 use aoc_traits::AdventOfCodeDay;
-use itertools::Itertools;
-use rustc_hash::{FxHashMap, FxHashSet};
 
 #[derive(Debug, Clone)]
-pub struct Grid {
-    harddrive: Vec<(usize, usize)>,
+pub struct HardDrive {
+    harddrive: Vec<Segment>,
 }
 
-impl FromStr for Grid {
+#[derive(Debug, Clone, Copy)]
+struct Segment {
+    id: usize,
+    start: usize,
+    size: usize,
+    free_after: usize,
+}
+
+impl FromStr for HardDrive {
     type Err = eyre::Report;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let harddrive = s
-            .chars()
-            .enumerate()
-            .map(|(idx, c)| (idx / 2, (c as u8 - b'0') as usize))
-            .collect::<Vec<_>>();
-        Ok(Grid { harddrive })
+        let mut start = 0;
+        let mut idx = 0;
+        let mut res = Vec::with_capacity(s.len() / 2 + 1);
+        for i in (0..s.len()).step_by(2) {
+            let size = (s.as_bytes()[i] as u8 - b'0') as usize;
+            let free_after =
+                (s.as_bytes().get(i + 1).copied().unwrap_or(b'0') as u8 - b'0' as u8) as usize;
+            res.push(Segment {
+                id: idx,
+                start,
+                size,
+                free_after,
+            });
+            start += size + free_after;
+            idx += 1;
+        }
+        Ok(HardDrive { harddrive: res })
     }
 }
-impl Grid {
+impl HardDrive {
     fn compress(&self) -> usize {
-        let mut result: VecDeque<_> = self.harddrive.iter().copied().collect();
+        let mut result: VecDeque<_> = self
+            .harddrive
+            .iter()
+            .flat_map(|x| [(x.id, x.size), (0, x.free_after)])
+            .collect();
+        result.pop_back();
 
         let mut res = 0;
         let mut current_hd_index = 0;
@@ -78,47 +100,33 @@ impl Grid {
         let mut result: VecDeque<_> = self.harddrive.iter().copied().collect();
 
         let mut res = 0;
-        let mut current_hd_index = 0;
-        let mut free = false;
-        loop {
-            if !free {
-                if result.is_empty() {
-                    break;
-                }
-                let (block_id, block_size) = result.pop_front().unwrap();
-                for _ in 0..block_size {
-                    res += current_hd_index * block_id;
-                    current_hd_index += 1;
-                }
-                free = true;
-            } else {
-                if result.is_empty() {
-                    break;
-                }
-                let (_free_id, free_size) = result.pop_front().unwrap();
-                if result.is_empty() {
-                    break;
-                }
-                // find the last block fitting space
-                if let Some((idx, &(block_id, block_size))) = {
-                    result
-                        .iter()
-                        .enumerate()
-                        .rev()
-                        .find(|(idx, (id, size))| *size <= free_size && *size > 0)
-                } {
-                    for _ in 0..block_size {
-                        res += current_hd_index * block_id;
-                        current_hd_index += 1;
+
+        'outer: while let Some(segment) = result.pop_back() {
+            // already handled this
+            if segment.id == 0 {
+                continue;
+            }
+            // try to find a fitting space
+            for i in 0..result.len() {
+                if result[i].free_after >= segment.size {
+                    let rest = result[i].free_after - segment.size;
+                    result[i].free_after = 0;
+                    let new = Segment {
+                        id: 0,
+                        start: result[i].start + result[i].size,
+                        size: segment.size,
+                        free_after: rest,
+                    };
+                    for i in 0..segment.size {
+                        res += segment.id * (new.start + i);
                     }
-                    if free_size > block_size {
-                        result.push_front((_free_id, free_size - block_size));
-                    }
-                } else {
-                    // nothing fits space, skip
-                    result.pop_front();
-                    free = false;
+                    result.insert(i + 1, new);
+                    continue 'outer;
                 }
+            }
+            // no fitting space found, do as is
+            for i in 0..segment.size {
+                res += segment.id * (segment.start + i);
             }
         }
         res
@@ -128,7 +136,7 @@ impl Grid {
 #[derive(Default)]
 pub struct Solver;
 impl AdventOfCodeDay for Solver {
-    type ParsedInput<'a> = Grid;
+    type ParsedInput<'a> = HardDrive;
     type Part1Output = usize;
     type Part2Output = usize;
 
@@ -140,7 +148,7 @@ impl AdventOfCodeDay for Solver {
         input.compress()
     }
     fn solve_part2(input: &Self::ParsedInput<'_>) -> Self::Part2Output {
-        0
+        input.compress2()
     }
 }
 
@@ -159,6 +167,6 @@ mod tests {
     #[test]
     fn test_stage2() {
         let parsed = Solver::parse_input(TEST_INPUT);
-        assert_eq!(Solver::solve_part2(&parsed), 34);
+        assert_eq!(Solver::solve_part2(&parsed), 2858);
     }
 }
