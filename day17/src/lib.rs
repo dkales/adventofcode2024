77a899422b5050f48409d2e6b4a2f827ec0f5989
Daniel Kales <11509575+dkales@users.noreply.github.com>
2024-12-19 18:07:16 +0100

    day17 part2

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Program {
     registers: [u64; 3],
     inst: Vec<u8>,
@@ -44,7 +44,7 @@ impl Program {
                 3 => 3,
                 4 => registers[0],
                 5 => registers[1],
-                7 => registers[2],
+                6 => registers[2],
                 _ => unreachable!(),
             }
         }
@@ -101,97 +101,167 @@ impl Program {
         }
         outputs.into_iter().join(",")
     }
-    fn brute_force_part2(&self) -> u64 {
-        #[inline]
-        fn combo(registers: [u64; 3], val: u8) -> u64 {
-            match val {
-                0 => 0,
-                1 => 1,
-                2 => 2,
-                3 => 3,
-                4 => registers[0],
-                5 => registers[1],
-                7 => registers[2],
-                _ => unreachable!(),
+
+    fn analyze_program(&self) -> u64 {
+        let mut instructions: Vec<_> = self.inst[..].chunks_exact(2).collect();
+        // some assertions for our program
+        assert!(
+            instructions.iter().filter(|inst| inst[0] == 3).count() == 1,
+            "we only have 1 jump"
+        );
+        assert!(
+            instructions.last().unwrap()[0] == 3 && instructions.last().unwrap()[1] == 0,
+            "jump is in the end and jumps to 0"
+        );
+        assert!(
+            instructions.iter().filter(|inst| inst[0] == 5).count() == 1,
+            "we only have 1 output"
+        );
+        assert!(
+            instructions[instructions.len() - 2][0] == 5,
+            "output is second to last instruction"
+        );
+        let output_register = match instructions[instructions.len() - 2][1] {
+            4 => A,
+            5 => B,
+            6 => C,
+            _ => panic!("output does not actually output a register"),
+        };
+        // pop print and jump instructions
+        instructions.pop();
+        instructions.pop();
+
+        let mut current_best_solution = u64::MAX;
+        let config = z3::Config::new();
+        loop {
+            let ctx = z3::Context::new(&config);
+            let solver = z3::Solver::new(&ctx);
+
+            // initial register values
+            const REGISTER_LEN: u32 = 64;
+            let reg_a = z3::ast::BV::new_const(&ctx, "reg_a", REGISTER_LEN);
+            let reg_b = z3::ast::BV::from_u64(
+                &ctx,
+                u64::try_from(self.registers[B]).expect("starting values are not larger than u64"),
+                REGISTER_LEN,
+            );
+            let reg_c = z3::ast::BV::from_u64(
+                &ctx,
+                u64::try_from(self.registers[C]).expect("starting values are not larger than u64"),
+                REGISTER_LEN,
+            );
+            let mut registers = [reg_a.clone(), reg_b, reg_c];
+            let bound = z3::ast::BV::from_u64(&ctx, current_best_solution, REGISTER_LEN);
+            solver.assert(&reg_a.bvult(&bound));
+
+            fn z3_combo<'a>(
+                ctx: &'a z3::Context,
+                registers: &[z3::ast::BV<'a>; 3],
+                val: u8,
+            ) -> z3::ast::BV<'a> {
+                match val {
+                    0 => z3::ast::BV::from_u64(&ctx, 0, REGISTER_LEN),
+                    1 => z3::ast::BV::from_u64(&ctx, 1, REGISTER_LEN),
+                    2 => z3::ast::BV::from_u64(&ctx, 2, REGISTER_LEN),
+                    3 => z3::ast::BV::from_u64(&ctx, 3, REGISTER_LEN),
+                    4 => registers[0].clone(),
+                    5 => registers[1].clone(),
+                    6 => registers[2].clone(),
+                    _ => unreachable!(),
+                }
             }
-        }
-        let want_outputs = self.inst.clone();
-        (0..=u64::MAX)
-            .into_par_iter()
-            .find_map_first(|i| {
-                let mut registers = self.registers;
-                registers[A] = i;
+            let and_mask = z3::ast::BV::from_u64(&ctx, 7, REGISTER_LEN);
 
-                let mut ip = 0;
-                let mut output_idx = 0;
-
-                while let Some(op) = self.inst.get(ip as usize).copied() {
-                    match op {
+            for wanted in self.inst.iter() {
+                for instruction in instructions.iter() {
+                    match instruction[0] {
                         // adv
                         0 => {
-                            let val = self.inst[ip + 1];
-                            registers[A] = registers[A] >> combo(registers, val);
+                            let val = instruction[1];
+                            let shift_amount = z3_combo(&ctx, &registers, val);
+                            let new_val = registers[A].bvlshr(&shift_amount);
+                            registers[A] = new_val;
                         }
                         // bxl
                         1 => {
-                            let val = self.inst[ip + 1];
-                            registers[B] ^= val as u64;
+                            let val = instruction[1];
+                            let new_val = registers[B].bvxor(&z3::ast::BV::from_u64(
+                                &ctx,
+                                val as u64,
+                                REGISTER_LEN,
+                            ));
+                            registers[B] = new_val;
                         }
                         // bst
                         2 => {
-                            let val = self.inst[ip + 1];
-                            registers[B] = combo(registers, val) % 8;
+                            let val = instruction[1];
+                            let val = z3_combo(&ctx, &registers, val);
+                            let new_val = val.bvand(&and_mask);
+                            registers[B] = new_val;
                         }
-                        //jnz
+                        // jmp
                         3 => {
-                            if registers[0] != 0 {
-                                let val = self.inst[ip + 1];
-                                ip = val as usize;
-                                continue;
-                            }
+                            panic!("jump instruction should not be here");
                         }
                         // bxc
                         4 => {
-                            registers[B] ^= registers[C];
+                            let new_val = registers[B].bvxor(&registers[C]);
+                            registers[B] = new_val;
                         }
                         // out
                         5 => {
-                            let val = self.inst[ip + 1];
-                            let out = combo(registers, val) % 8;
-                            if output_idx >= want_outputs.len()
-                                || out as u8 != want_outputs[output_idx]
-                            {
-                                return None;
-                            }
-                            output_idx += 1;
+                            panic!("output instruction should not be here");
                         }
                         // bdv
                         6 => {
-                            let val = self.inst[ip + 1];
-                            registers[B] = registers[A] >> combo(registers, val);
+                            let val = instruction[1];
+                            let shift_amount = z3_combo(&ctx, &registers, val);
+                            let new_val = registers[A].bvlshr(&shift_amount);
+                            registers[B] = new_val;
                         }
                         // cdv
                         7 => {
-                            let val = self.inst[ip + 1];
-                            registers[C] = registers[A] >> combo(registers, val);
+                            let val = instruction[1];
+                            let shift_amount = z3_combo(&ctx, &registers, val);
+                            let new_val = registers[A].bvlshr(&shift_amount);
+                            registers[C] = new_val;
                         }
                         _ => unreachable!(),
                     }
-                    ip += 2;
                 }
-                if output_idx == want_outputs.len() {
-                    return Some(i);
-                }
-                None
-            })
-            .unwrap()
+
+                // after instructions, we get an output
+                let wanted = z3::ast::BV::from_u64(&ctx, *wanted as u64, REGISTER_LEN);
+                let actual = registers[output_register].bvand(&and_mask);
+                solver.assert(&wanted._eq(&actual));
+            }
+            // we also want the a register to be 0, since this is the end of the program
+            solver.assert(&registers[A]._eq(&z3::ast::BV::from_u64(&ctx, 0, REGISTER_LEN)));
+
+            if solver.check() != z3::SatResult::Sat {
+                break;
+            }
+            let model = solver
+                .get_model()
+                .expect("have a model, since we checked for sat");
+            let res = model
+                .get_const_interp(&reg_a)
+                .expect("register A should be in the model");
+            let potential_res = res
+                .as_u64()
+                .expect("register A should be a u64 in the model");
+            current_best_solution = potential_res;
+        }
+        current_best_solution
     }
 }
+
+use core::panic;
 
 use aoc_traits::AdventOfCodeDay;
 use itertools::Itertools;
 use nom::{bytes::complete::tag, IResult};
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use z3::ast::Ast;
 pub struct Solver;
 impl AdventOfCodeDay for Solver {
     type ParsedInput<'a> = Program;
@@ -206,7 +276,7 @@ impl AdventOfCodeDay for Solver {
         input.execute()
     }
     fn solve_part2(input: &Self::ParsedInput<'_>) -> Self::Part2Output {
-        input.brute_force_part2()
+        input.analyze_program()
     }
 }
 
